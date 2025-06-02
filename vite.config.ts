cac934aaec232b4bbf7cf5b27f0a6754bfeb59b7
Saplyn Miao <saplynx@outlook.com>
2025-06-02 22:35:24 +1000

    feat: setup tailwindcss

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueDevTools from "vite-plugin-vue-devtools";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,6 +12,7 @@ export default defineConfig({
     vueDevTools({
       launchEditor: "nvim",
     }),
+    tailwindcss(),
   ],
   resolve: {
     alias: {
